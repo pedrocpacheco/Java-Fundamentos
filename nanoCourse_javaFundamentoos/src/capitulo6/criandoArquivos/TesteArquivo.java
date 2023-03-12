4708eb6fab38eccb4fcd2180e464dbe8b334eec0
Pedro C. Pacheco <cpedropacheco@hotmail.com>
2023-03-12 15:50:04 -0300

    FileReader and BufferedReader

--- nanoCourse_javaFundamentoos/src/capitulo6/criandoArquivos/TesteArquivo.java
+++ nanoCourse_javaFundamentoos/src/capitulo6/criandoArquivos/TesteArquivo.java
@@ -1,5 +1,7 @@
 package capitulo6.criandoArquivos;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -10,22 +12,26 @@ public class TesteArquivo {
 	
 	public static void gravarEstoque(String nomeArquivo, String caminho, List<String> conteudo) {
 		
+		// Criando variaveis do tipo File Writer e PrintWriter (classes)
 		FileWriter stream;
 		PrintWriter print;
 		
 		try {
 			
-			stream = new FileWriter(caminho); 
-			print = new PrintWriter(stream);
-			
+			// atribuindo variaveis a objetos da mesma classe do tipo da variavel
+			stream = new FileWriter(caminho); // criando o arquivo com o nome definido anteriomente
+			print = new PrintWriter(stream); // passando o arquivo onde iremos printar
+
+			// Fazendo um foreach por todos os Arrays da nossa lista de Arrays
 			for(String linha: conteudo) {
 				print.println(linha);
 			}
 			
+			// Fechando os objetos
 			stream.close();
 			print.close();
 			
-		
+		  // Printando um erro IOException caso erro no local de arquivo
 		} catch(IOException e) {
 			e.printStackTrace();
 			
@@ -36,17 +42,43 @@ public class TesteArquivo {
 	}
 
 	public static void lerArquivo(String caminho) {
-		
-		
+		try {
+			
+			// Abre o arquivo para a leitura
+			FileReader stream = new FileReader(caminho);
+			
+			// Passamos o arquivo para leitura a um objeto capaz de lelo
+			BufferedReader reader = new BufferedReader(stream);
+			
+			// Criando o leitor de uma linha do arquivo para leitura
+			String linha = reader.readLine(); // lendo uma linha
+			
+			// Lendo todas as linhas atraves de uma estrutura de repeteicao
+			while (linha != null) { // o valor de linha só será null quando a linha não tiver conteudo
+				
+				System.out.println(linha); // le a linha atual
+				linha = reader.readLine(); // passando para a proxima linha
+			
+			}
+			
+			reader.close();
+			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+			
+		}
 		
 	}
 	
 	public static void main(String[] args) {
 		
+		// Criando caminho para o arquivo
 		String nomeArquivo = "estoque.csv";
 		String diretorio = System.getProperty("user.home");
 		String caminho = diretorio + "\\" + nomeArquivo; 
 		
+		// Criando uma lista de arrays com o conteudo do nosso arquivo
 		List<String> conteudo = new ArrayList<>();
 		conteudo.add("Serie; Ator Principal; Temporadas; Nota");
 		conteudo.add("The Walking Dead; Andrew Lincoln; 11; 8.7");
@@ -55,7 +87,9 @@ public class TesteArquivo {
 		conteudo.add("Breaking Bad; Bryan Crasnton; 5; 9.2" );
 		
 		// Criando função que recebe 3 atributos
-		gravarEstoque(nomeArquivo, caminho, conteudo);
+		// gravarEstoque(nomeArquivo, caminho, conteudo);
+		
+		lerArquivo(caminho);
 		
 	}
 	

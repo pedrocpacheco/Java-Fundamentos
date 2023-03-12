a2bfa78d435f544f8ef32bad410b164336deca0d
Pedro C. Pacheco <cpedropacheco@hotmail.com>
2023-03-12 12:05:05 -0300

    criando excecoes cheked e uncheked

--- nanoCourse_javaFundamentoos/src/capitulo6/criandoExcecoes/CriandoExcecoes.java
+++ nanoCourse_javaFundamentoos/src/capitulo6/criandoExcecoes/CriandoExcecoes.java
@@ -5,10 +5,13 @@ public class CriandoExcecoes {
 	public static void main(String[] args) {
 		
 	try {	
+		
 		int val = 10 / 0;
 		System.out.println(val);
+		
 	} catch (ArithmeticException e) {
 		
+		// Quando rodar o programa sera lançado no console
 		throw new DivisaoPorZeroException();
 		
 	}
